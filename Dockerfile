ae2d7d5b898009a2efb7c1b67cb1efcc540592e4
Serge <8523760+sergethedev17@users.noreply.github.com>
2021-01-31 20:51:44 +1100

    Add web server SSL support

--- Dockerfile
+++ Dockerfile
@@ -6,12 +6,14 @@ ENV DOH_URL="https://doh.cleanbrowsing.org/doh/security-filter/"
 ENV DOH_SERVE_PORT=5053
 ENV DNS=127.0.0.1#5053
 
+ENV SSL_HOSTNAME=
+ENV SSL_CERTIFICATE=
+
 COPY build/* /tmp/build/
 
-RUN /bin/bash -c 'wget -q -O ./cloudflared.tgz $DOH_BINARY_TGZ_URL && \
-tar -xvzf ./cloudflared.tgz && \
-rm ./cloudflared.tgz && \
-mkdir -p /tmp/build/bin && \
-mv ./cloudflared /tmp/build/bin && \
-/tmp/build/doh.sh && \
-rm -rf /tmp/build'
+RUN /bin/bash -c 'mkdir -p /tmp/build && cd /tmp/build && \
+	wget -q -O ./cloudflared.tgz $DOH_BINARY_TGZ_URL && \
+	tar -xvzf ./cloudflared.tgz && \
+	mkdir ./bin && mv ./cloudflared ./bin && \
+	/tmp/build/doh.sh && \
+	rm -rf /tmp/build'
--- build/doh.sh
+++ build/doh.sh
@@ -3,6 +3,10 @@
 # exit when any command fails
 set -e
 
+# Clean up rubbish files
+rm -rf /www/html/admin/.git
+
+# Prepare DOH daemon
 useradd -s /usr/sbin/nologin -r -M cloudflared
 mv ./bin/cloudflared /usr/local/bin
 chmod +x /usr/local/bin/cloudflared
@@ -12,4 +16,11 @@ chown cloudflared:cloudflared /usr/local/bin/cloudflared
 
 mkdir -p /etc/s6/services/cloudflared
 mv ./s6_services_cloudflared_run /etc/s6/services/cloudflared/run
-chmod +x /etc/s6/services/cloudflared/run
+chmod +rx /etc/s6/services/cloudflared/run
+
+
+
+# Prepare Lighttpd SSL, if configured
+mv ./lighttpd_external.conf.template /etc/lighttpd/external.conf.template
+mv ./lighttpd-prep /etc/cont-init.d/lighttpd-prep
+chmod 755 /etc/cont-init.d/lighttpd-prep
